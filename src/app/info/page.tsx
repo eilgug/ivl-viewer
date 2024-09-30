ae4cd27b1f17a6528a7b03ca86f631129480bdc3
eilgug <rguglielmin.995@gmail.com>
2024-09-30 19:26:22 +0200

    fix searchParams in Suspense

--- src/app/info/page.tsx
+++ src/app/info/page.tsx
@@ -4,27 +4,33 @@ import NextMatch from "@/components/NextMatch";
 import { useSearchParams } from "next/navigation";
 import Standing from "@/components/Standing";
 
-const InfoPage: React.FC = async () => {
+const InfoPageContent: React.FC = () => {
     const searchParams = useSearchParams();
 
-    const season: number = parseInt(searchParams.get("season") as string);
+    const season: number = parseInt(searchParams.get("season") || "0", 0);
     // const territoryId: number = parseInt(searchParams.get("territory") as string);
     // const championshipId: number = parseInt(searchParams.get("championship") as string);
-    const groupId: number = parseInt(searchParams.get("group") as string);
-    const teamId: number = parseInt(searchParams.get("team") as string);
+    const groupId: number = parseInt(searchParams.get("group") || "0", 0);
+    const teamId: number = parseInt(searchParams.get("team") || "0", 0);
 
     return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <div className="container vh-100">
-                <main>
-                    <div className="my-3">
-                        <NextMatch teamId={teamId} groupId={groupId} season={season} />
-                        <Standing season={season} groupId={groupId} />
-                    </div>
-                </main>
-            </div>
-        </Suspense>
+        <div className="container vh-100">
+            <main>
+                <div className="my-3">
+                    <NextMatch teamId={teamId} groupId={groupId} season={season} />
+                    <Standing season={season} groupId={groupId} />
+                </div>
+            </main>
+        </div>
     );
 }
+
+const InfoPage: React.FC = () => {
+    return (
+        <Suspense fallback={<div>Loading page...</div>}>
+            <InfoPageContent />
+        </Suspense>
+    );
+};
 
 export default InfoPage;
